e80fd87d6af0743eaba090bee1fcbbf7392a5999
Jacob Mompean <jacob.mompean@alliantgroup.com>
2026-01-29 14:51:12 -0600

    add path validation to view, edit, and save handlers

--- wiki.go
+++ wiki.go
@@ -21,7 +21,7 @@ func (p *Page) save() error {
 
 var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
 
-func getTitle(w, httpResponseWriter, r *http.Request) (string, error) {
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
   tit := validPath.FindStringSubmatch(r.URL.Path)
   if tit != nil {
     http.NotFound(w, r)
@@ -50,7 +50,10 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/view/"):]
+	title, err := getTitle(w, r)
+  if err != nil {
+    return
+  }
 	p, err := loadPage(title)
   if err != nil {
     http.Redirect(w, r, "/edit/"+title, http.StatusFound)
@@ -60,7 +63,10 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/edit/"):]
+	title, err := getTitle(w, r)
+  if err != nil {
+    return
+  }
 	p, err := loadPage(title)
 	if err != nil {
 		p = &Page{Title: title}
@@ -69,10 +75,13 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func saveHandler(w http.ResponseWriter, r *http.Request) {
-  title := r.URL.Path[len("/save/"):]
+  title, err := getTitle(w, r)
+  if err != nil {
+    return
+  }
   body := r.FormValue("body")
   p := &Page{Title: title, Body: []byte(body)}
-  err := p.save()
+  err = p.save()
   if err != nil {
     http.Error(w, err.Error(), http.StatusInternalServerError)
     return
